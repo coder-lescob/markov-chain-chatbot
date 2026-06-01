e1a6e75de240e08cb86847630a895dc07c1d9a1e
coder-lescob <gabriel.lescob@outlook.com>
2026-06-01 16:12:18 +0200

    * renamed main.py training.py * made it count things

--- training.py
+++ training.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import json
 
 type freq = float
 type proba = float
@@ -24,7 +25,7 @@ class MarkovChainAnalyser:
         return -1
 
     def analyse_text(self, text: str):
-        words = text.lower().replace(",", " , ").replace(".", " . ").replace("!", " ! ").replace("?", " ? ").split()
+        words = text.lower().replace(",", " , ").replace(".", " . ").replace("!", " ! ").replace("?", " ? ").replace("'", " ' ").split()
 
         for word, next_word in zip(words, words[1::]):
             if (i := self.idx_of_word_in_dict(word)) == -1:
@@ -32,25 +33,35 @@ class MarkovChainAnalyser:
                 self.dictionnary.append(Token(word, 0, {}))
 
             self.dictionnary[i].frequency += 1
-            print(self.dictionnary[i].frequency, i)
             next_idx = self.idx_of_word_in_dict(next_word)
+
             if next_idx == -1:
                 next_idx = len(self.dictionnary)
                 self.dictionnary.append(Token(next_word, 0, {}))
 
-            print(self.dictionnary[i].frequency, i)
             if self.dictionnary[i].next_words.get(next_idx) is None:
                 self.dictionnary[i].next_words[next_idx] = [ 0, 0 ]
 
             self.dictionnary[i].next_words[next_idx][0] += 1
             self.dictionnary[i].next_words[next_idx][1] = self.dictionnary[i].next_words[next_idx][0] / self.dictionnary[i].frequency
 
-            
+    def toJson(self):
+        return [
+            {
+                "word": token.word,
+                "frequency": token.frequency,
+                "next_words": token.next_words
+            }
 
-        print(self.dictionnary)
+            for token in self.dictionnary
+        ]
+
 def main():
     makov_analyser = MarkovChainAnalyser()
-    makov_analyser.analyse_text("hello, world !")
+    with open("training text.txt", "r") as file:
+        makov_analyser.analyse_text(file.read())
 
+    with open("output.json", "w") as file:
+        file.write(json.dumps(makov_analyser.toJson(), indent=4))
 if __name__ == "__main__":
     main()
